f5a52d87a14df67095fee651dee0f61993e5009f
Djennelbaroud <gh_djennelbaroud@esi.dz>
2021-05-27 20:56:41 +0100

    Initial Commit

---
+++ Price Converter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Price Converter
+//
+//  Created by Djennelbaroud Hadj Cheikh on 27/05/2021.
+//  Copyright © 2021 Djennelbaroud Hadj Cheikh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Price ConverterTests/Price_ConverterTests.swift
@@ -0,0 +1,34 @@
+//
+//  Price_ConverterTests.swift
+//  Price ConverterTests
+//
+//  Created by Djennelbaroud Hadj Cheikh on 27/05/2021.
+//  Copyright © 2021 Djennelbaroud Hadj Cheikh. All rights reserved.
+//
+
+import XCTest
+@testable import Price_Converter
+
+class Price_ConverterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
