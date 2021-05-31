04d24290bb94ea61a6d28ac3da9333938f92f798
Djennelbaroud <gh_djennelbaroud@esi.dz>
2021-05-31 03:21:48 +0100

    first version of app  commited

---
+++ Price Converter/Model/ConvertedCurrencyData.swift
@@ -0,0 +1,9 @@
+//
+//  ConvertedCurrencyData.swift
+//  Price Converter
+//
+//  Created by Djennelbaroud Hadj Cheikh on 29/05/2021.
+//  Copyright © 2021 Djennelbaroud Hadj Cheikh. All rights reserved.
+//
+
+import Foundation
